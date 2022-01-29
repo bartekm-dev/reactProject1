6bf7e6c24e72e65817188428ceec1377286dae2b
Bartosz Makowski <bartoszmakowski@Bartoszs-Air.broadband>
2022-01-29 22:35:34 +0000

    lesson 6 done

--- bjj-blog/src/App.js
+++ bjj-blog/src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
+
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
+  const title = 'Welcome to the new BJJ Blog';
+  const likes = 50;
+  let site = 'www.google.com';
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>App Component</h1>
+      <Home />
+        <h1>{ title }</h1>
+        <p>Likes: { likes }</p>
+        <a href={ site }>Google Site</a>
       </div>
     </div>
   );
